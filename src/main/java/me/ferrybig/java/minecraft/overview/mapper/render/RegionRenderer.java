7f84b2aef9152315e4c6e3773ad296b7bee741d0
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2014-11-07 23:45:58 +0100

    Added interface for File name + inputstream --> BufferedImage

---
+++ src/main/java/me/ferrybig/java/minecraft/overview/mapper/render/RegionRenderer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package me.ferrybig.java.minecraft.overview.mapper.render;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface RegionRenderer {
+    public BufferedImage renderFile(String fileName, InputStream in) 
+            throws IOException;
+}
