2ebac3d79507d17547377385b4a7937f6811f58f
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2014-11-18 15:56:34 -0100

    Updated simplerenderer

--- src/main/java/me/ferrybig/java/minecraft/overview/mapper/render/SimpleRenderer.java
+++ src/main/java/me/ferrybig/java/minecraft/overview/mapper/render/SimpleRenderer.java
@@ -3,16 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package me.ferrybig.java.minecraft.overview.mapper.render;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jnbt.CompoundTag;
+import org.jnbt.NBTInputStream;
 
 public abstract class SimpleRenderer implements RenderEngine {
 
+    private static final Pattern rfpat
+        = Pattern.compile("^r\\.(-?\\d+)\\.(-?\\d+)\\.mca$");
     private final RegionRenderer renderer;
 
     public SimpleRenderer(RegionRenderer renderer) {
@@ -21,11 +25,19 @@ public abstract class SimpleRenderer implements RenderEngine {
 
     @Override
     public void addFile(String fileName, InputStream in) throws IOException {
-        renderer.renderFile(fileName, in);
+        Matcher localMatcher;
+        if ((localMatcher = rfpat.matcher(fileName)).matches()) {
+            addImage(renderer.renderFile(fileName, in),
+                Integer.parseInt((localMatcher.group(1))),
+                Integer.parseInt((localMatcher.group(2))));
+        } else if (fileName.equals("level.dat")) {
+            this.addLevelDat((CompoundTag) new NBTInputStream(in).readTag());
+        }
     }
-    
+
     protected abstract void addLevelDat(CompoundTag level) throws IOException;
-    
-    protected abstract void addImage(BufferedImage tile, int x, int z) throws IOException;
+
+    protected abstract void addImage(BufferedImage tile, int x, int z)
+        throws IOException;
 
 }
