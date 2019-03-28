8f642ad5de16dab08c34b33d1d84494c274fc230
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2019-03-28 22:20:50 +0100

    Make logic inside ParallelTaskRunner<T> better

--- src/main/java/me/ferrybig/java/minecraft/overview/mapper/engine/ParallelTaskRunner.java
+++ src/main/java/me/ferrybig/java/minecraft/overview/mapper/engine/ParallelTaskRunner.java
@@ -6,6 +6,7 @@
 package me.ferrybig.java.minecraft.overview.mapper.engine;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
@@ -13,33 +14,57 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntFunction;
 import java.util.function.IntSupplier;
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class ParallelTaskRunner<T> {
 
+	@Nonnull
 	private final AtomicInteger fileIndex = new AtomicInteger();
+	@Nonnull
 	private final ExecutorService pool;
 	private final int maxTasks;
 	private final int totalTasks;
 	private int doneWorkers = 0;
 	private volatile boolean isCancelled = false;
+	@Nonnull
 	private final List<Throwable> exceptions = new CopyOnWriteArrayList<>();
+	@Nonnull
 	private final IntFunction<T> getTask;
+	@Nonnull
 	private final ExceptionConsumer<T> runTask;
 
-	public ParallelTaskRunner(ExecutorService pool, int maxTasks, List<T> tasks, ExceptionConsumer<T> runTask) {
+	public ParallelTaskRunner(
+		@Nonnull ExecutorService pool,
+		@Nonnegative int maxTasks,
+		@Nonnull List<T> tasks,
+		@Nonnull ExceptionConsumer<T> runTask
+	) {
 		this(pool, maxTasks, tasks::size, tasks::get, runTask);
 	}
 
-	public ParallelTaskRunner(ExecutorService pool, int maxTasks, T[] tasks, ExceptionConsumer<T> runTask) {
+	public ParallelTaskRunner(
+		@Nonnull ExecutorService pool,
+		@Nonnegative int maxTasks,
+		@Nonnull T[] tasks,
+		@Nonnull ExceptionConsumer<T> runTask
+	) {
 		this(pool, maxTasks, () -> tasks.length, index -> tasks[index], runTask);
 	}
 
-	public ParallelTaskRunner(ExecutorService pool, int maxTasks, IntSupplier totalTasks, IntFunction<T> getTask, ExceptionConsumer<T> runTask) {
-		this.pool = pool;
-		this.maxTasks = maxTasks;
+	public ParallelTaskRunner(
+		@Nonnull ExecutorService pool,
+		@Nonnegative int maxTasks,
+		@Nonnull IntSupplier totalTasks,
+		@Nonnull IntFunction<T> getTask,
+		@Nonnull ExceptionConsumer<T> runTask
+	) {
+		this.pool = Objects.requireNonNull(pool, "pool");
 		this.totalTasks = totalTasks.getAsInt();
-		this.getTask = getTask;
-		this.runTask = runTask;
+		this.maxTasks = Math.max(1, Math.min(maxTasks, this.totalTasks));
+		this.getTask = Objects.requireNonNull(getTask, "getTask");
+		this.runTask = Objects.requireNonNull(runTask, "runTask");
 	}
 
 	private T newTask() throws InterruptedException {
@@ -53,7 +78,7 @@ public class ParallelTaskRunner<T> {
 		return null;
 	}
 
-	private void onWorkerDone(Throwable ex) {
+	private void onWorkerDone(@Nullable Throwable ex) {
 		synchronized (this) {
 			if (ex != null) {
 				this.exceptions.add(ex);
@@ -67,6 +92,7 @@ public class ParallelTaskRunner<T> {
 	}
 
 	private void taskRunner() {
+		@Nullable
 		Throwable exception = null;
 		try {
 			T task;
@@ -84,8 +110,9 @@ public class ParallelTaskRunner<T> {
 		onWorkerDone(exception);
 	}
 
+	@Nonnull
 	public ParallelTaskRunner<T> start() {
-		int toSpawn = Math.max(1, Math.min(this.maxTasks, this.totalTasks));
+		int toSpawn = this.maxTasks;
 		for (int i = 0; i < toSpawn; i++) {
 			this.pool.submit(this::taskRunner);
 		}
@@ -108,10 +135,40 @@ public class ParallelTaskRunner<T> {
 		}
 	}
 
-	public void waitForResult() throws CancellationException, InterruptedException, ExecutionException {
+	private boolean isDone0() {
+		return this.doneWorkers != this.maxTasks;
+	}
+
+	public boolean isDone() {
 		synchronized (this) {
-			boolean interrupted = false;
-			while (this.doneWorkers != this.maxTasks) {
+			return this.isDone0();
+		}
+	}
+
+	private void getResult() throws CancellationException, ExecutionException {
+		assert this.isDone0();
+		int exceptionsSize = this.exceptions.size();
+		if (exceptionsSize > 0) {
+			ExecutionException ex = new ExecutionException(this.exceptions.get(0));
+			for (int i = 1; i < exceptionsSize; i++) { // Start loop at 1 so we skip the first exception
+				ex.addSuppressed(this.exceptions.get(i));
+			}
+			throw ex;
+		}
+		if (this.isCancelled) {
+			throw new CancellationException();
+		}
+		if (this.fileIndex.get() < this.totalTasks) {
+			throw new ExecutionException(
+				new IllegalStateException("Pool didn't execute all tasks, " + this.fileIndex.get() + " executed, vs " + this.totalTasks + " total")
+			);
+		}
+	}
+
+	public void waitForResult() throws CancellationException, InterruptedException, ExecutionException {
+		boolean interrupted = false;
+		synchronized (this) {
+			while (isDone0()) {
 				try {
 					this.wait();
 				} catch (InterruptedException ex) {
@@ -119,26 +176,11 @@ public class ParallelTaskRunner<T> {
 					interrupted = true;
 				}
 			}
-			if (interrupted) {
-				throw new InterruptedException();
-			}
-			if (this.isCancelled) {
-				throw new CancellationException();
-			}
-			int exceptionsSize = this.exceptions.size();
-			if (exceptionsSize > 0) {
-				ExecutionException ex = new ExecutionException(this.exceptions.get(0));
-				for (int i = 1; i < exceptionsSize; i++) { // Start loop at 1 so we skip the first exception
-					ex.addSuppressed(this.exceptions.get(i));
-				}
-				throw ex;
-			}
-			if (this.fileIndex.get() < this.totalTasks) {
-				throw new ExecutionException(
-					new IllegalStateException("Pool didn't execute all tasks, " + this.fileIndex.get() + " executed, vs " + this.totalTasks + " total")
-				);
-			}
 		}
+		if (interrupted) {
+			throw new InterruptedException();
+		}
+		this.getResult();
 	}
 
 	public static interface ExceptionConsumer<T> {
